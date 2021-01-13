f9b1d212146ea75262b9f28ae83290cbbcf2fa09
betakurnia <betakurnia1903@gmail.com>
2021-01-13 17:57:03 +0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,31 @@
+const { Telegraf } = require("telegraf");
+const feedParser = require("ortoo-feedparser");
+var cron = require("node-cron");
+
+const url = "https://www.livechart.me/feeds/episodes";
+
+const bot = new Telegraf();
+
+const articles = [];
+
+feedParser.parseUrl(url).on("article", (article) => {
+  articles.push(article);
+});
+
+bot.start((ctx) => {
+  cron.schedule("5 * * * * *", () => {
+    articles.forEach((article) => {
+      if (
+        Date.parse(new Date()) - 60000 < Date.parse(article.pubDate) &&
+        Date.parse(new Date()) > Date.parse(article.pubDate)
+      ) {
+        ctx.reply(article.title);
+      }
+    });
+  });
+});
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
